27ae4bfd7b435ea7c757a0c688e9ea315fbb0a83
edcastillob <edwar.castillo@gmail.com>
2023-11-12 16:42:40 -0400

    Renderización tabla Department

--- src/components/data-table/table.tsx
+++ src/components/data-table/table.tsx
@@ -1,14 +1,88 @@
 "use client";
 
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+ 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 const DataTable: React.FC<{ data: any[] }> = ({ data }) => {
  
 
   console.log(data);
+  const columns: ColumnDef<any, any>[] = [
+    {
+      header: "ID Departamento",
+      accessorKey: "id_department",
+    },
+    {
+      header: "Nombre",
+      accessorKey: "name",
+    },
+  ];
   
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
   return (
-    <div>Tabla</div>
-  );
-};
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+  {header.isPlaceholder
+    ? null
+    : flexRender(
+        header.column.columnDef.header,
+        header.getContext()
+      )}
+</TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                 <TableCell key={cell.id}>
+                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
+               </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
 
 export default DataTable;
